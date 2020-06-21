b9d0b8e792596a9f8934248ce41bc837e6df60ce
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-21 10:01:50 -0700

    Add recipes to recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'a description for recipe','https://www.wholesomeyum.com/wp-content/uploads/2019/09/wholesomeyum-keto-chaffles-recipe-sweet-savory-5-ways-24.jpg'),
+    new Recipe('Cold Coco', 'A recipe to make a cold coco', 'https://lh3.googleusercontent.com/proxy/iqZ7poYwzp0ZxbpXRVJfHlBlaCsfkF3XBlslSvQObRcki83vPBNRQAfylJrQkCq2ujvtSXXWsgokBhMpw3YWaZ39p60T3RhKYsY2PBNJo3yk5gENCkg4kcB082k')
+  ]
 
   constructor() { }
 
